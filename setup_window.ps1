f5bc4de7f1d86d23bcfe4e88374d7a01bb077140
Long18 <us.thanhlong18@gmail.com>
2024-05-12 11:37:30 +0700

    fix: correct typo and spacing code

--- setup_window.ps1
+++ setup_window.ps1
@@ -21,7 +21,11 @@ function Write-Done {
 }
 
 # Start
-Start-Process -Wait powershell -Verb runas -ArgumentList "Set-ItemPropety -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0"
+Start-Process -Wait powershell -Verb runas -ArgumentList @"
+    Set-ItemPropety -Path 
+    REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System 
+    -Name ConsentPromptBehaviorAdmin -Value 0
+"@
 
 Write-Start -msg "Installing Scoop..."
 if (Get-Command scoop -ErrorAction SilentlyContinue) {
@@ -33,7 +37,7 @@ else {
 }
 Write-Done
 
-Install packages
+# Install packages
 Write-Start -msg "Installing Scoop packages..."
 scoop install git
 scoop bucket add main
@@ -76,8 +80,8 @@ if (-not (Test-Path $DestinationPath)) {
     New-Item -Path $DestinationPath -ItemType Directory -Force
 }
 Copy-Item -Path ".\neovim\*" -Destination $DestinationPath -Force -Recurse
-iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | `
-    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
+Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | `
+    New-Item "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 nvim -E -s -u "$($env:USERPROFILE)\AppData\Local\nvim\init.vim" +PlugInstall +PlugUpdate +q
 Write-Done
 
@@ -88,7 +92,7 @@ $PowershellPath = "$env:USERPROFILE\Documents\PowerShell"
 if (-not (Test-Path $ClinkPath)) {
     New-Item -Path $ClinkPath -ItemType Directory -Force
 }
-if(-not (Test-Path $PowershellPath)) {
+if (-not (Test-Path $PowershellPath)) {
     New-Item -Path $PowershellPath -ItemType Directory -Force
 }
 Copy-Item -Path ".\terminal\oh-my-posh.lua" -Destination $ClinkPath -Force
@@ -109,7 +113,7 @@ Write-Done
 
 # Install WSL
 Write-Start -msg "Installing WSL..."
-If(!(wsl -l -v)){
+If (!(wsl -l -v)) {
     wsl --install
     wsl --update
     wsl --install --no-launch --web-download -d Ubuntu
