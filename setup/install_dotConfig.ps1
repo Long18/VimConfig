964d31588912351967f136bac17a095a03a3543f
Long18 <us.thanhlong18@gmail.com>
2024-05-12 12:53:16 +0700

    feat(dotfiles): vim for vscode

--- setup/install_dotConfig.ps1
+++ setup/install_dotConfig.ps1
@@ -14,5 +14,14 @@ $ConfigPath = "$env:USERPROFILE\.config"
 if (-not (Test-Path $ConfigPath)) {
     New-Item -Path $ConfigPath -ItemType Directory -Force
 }
-Copy-Item -Path ".\config\*" -Destination $ConfigPath -Force -Recurse
+Copy-Item -Path ".\.config\winfetch\*" -Destination $ConfigPath -Force -Recurse
+Write-Done
+
+# Configure vim for vscode
+Write-Start -msg "Installing vim for vscode..."
+$DestinationPath = "$env:USERPROFILE\.vimrc"
+if (-not (Test-Path $DestinationPath)) {
+    New-Item -Path $DestinationPath -ItemType Directory -Force
+}
+Copy-Item -Path ".\.config\vim\.vimrc" -Destination $DestinationPath -Force
 Write-Done
