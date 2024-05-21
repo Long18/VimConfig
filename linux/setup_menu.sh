4d84b04a3db99d63e8f822ef34166ed03f1150a6
Long18 <us.thanhlong18@gmail.com>
2024-05-21 19:11:42 +0700

    refactor(set-up): make option for linux

---
+++ linux/setup_menu.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Welcome to the Linux setup menu!"
+echo "Please select an option:"
+echo "a. Install Everything"
+echo "1. Install Packages"
+echo "2. Install Tools"
+echo "3. Install Dot Files"
+echo "q. Quit"
+
+echo -n "Enter your choice: "
+read -n 1 -r choice
+echo ""
+
+case "$choice" in
+a | A)
+    echo "Installing Everything..."
+    # init everything we have here
+    ;;
+1)
+    echo "Installing Packages..."
+    # ./linux/install_packages.sh
+    ;;
+2)
+    echo "Installing Tools..."
+    # ./linux/install_tools.sh
+    ;;
+3)
+    echo "Installing Dot Files..."
+    # ./linux/install_dot_files.sh
+    ;;
+q | Q)
+    echo "Exiting..."
+    exit 0
+    ;;
+*)
+    echo "Invalid choice. Exiting..."
+    exit 1
+    ;;
+esac
--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@
 if [[ "$OSTYPE" == "msys" ]]; then
     pwsh ./windows/setup_window.ps1
 else
-    echo 'Processing for Linux'
-    # TODO: Add your Linux processing logic here
+    ./linux/setup_menu.sh
 fi
