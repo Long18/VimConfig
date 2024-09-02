1302a2b42e72cca10aa18f6acd7059f178ff42be
William <us.thanhlong18@gmail.com>
2024-09-02 14:53:13 +0700

    fix: path

--- windows/install_tools.ps1
+++ windows/install_tools.ps1
@@ -14,7 +14,7 @@ $DestinationPath = "$env:USERPROFILE\AppData\Local\nvim"
 if (-not (Test-Path $DestinationPath)) {
     New-Item -Path $DestinationPath -ItemType Directory -Force
 }
-Copy-Item -Path ".\neovim\*" -Destination $DestinationPath -Force -Recurse
+Copy-Item -Path "..\neovim\*" -Destination $DestinationPath -Force -Recurse
 Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | `
     New-Item "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 nvim -E -s -u "$($env:USERPROFILE)\AppData\Local\nvim\init.vim" +PlugInstall +PlugUpdate +q
@@ -30,8 +30,8 @@ if (-not (Test-Path $ClinkPath)) {
 if (-not (Test-Path $PowershellPath)) {
     New-Item -Path $PowershellPath -ItemType Directory -Force
 }
-Copy-Item -Path ".\terminals\oh-my-posh.lua" -Destination $ClinkPath -Force
-Copy-Item -Path ".\terminals\Microsoft.PowerShell_profile.ps1" -Destination $PowershellPath -Force
+Copy-Item -Path "..\terminals\oh-my-posh.lua" -Destination $ClinkPath -Force
+Copy-Item -Path "..\terminals\Microsoft.PowerShell_profile.ps1" -Destination $PowershellPath -Force
 Write-Done
 
 # Configure Vscode
